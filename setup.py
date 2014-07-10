8921504542644089ea5c200f8856bc0058d8051b
TalissaJoly <talissa.joly@hp.com>
2014-07-10 09:05:53 +0200

    setup testing with tox

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+
+from setuptools import setup, find_packages
+
+packages = [
+    'housenum-be-r-parser',
+]
+
+requires = [
+    'nose'
+]
+
+setup(
+    name='housenum-be-r-parser',
+    version='',
+    description='',
+    long_description='',
+    author='Onroerend Erfgoed',
+    author_email='ict@onroerenderfgoed.be',
+    url='http://github.com/onroerenderfgoed/housenum-be-r-parser',
+    packages=find_packages(),
+    package_data={'': ['LICENSE']},
+    package_dir={'housenum-be-r-parser': 'housenum-be-r-parser'},
+    include_package_data=True,
+    install_requires=requires,
+    license='MIT',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    test_suite='tox'
+)
